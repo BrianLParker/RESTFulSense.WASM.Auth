ac3a6bb2d5370000272ed7e77eb4a3bf04b985c2
Brian Parker <brian.parker@hotmail.com.au>
2021-12-12 05:14:52 +1100

    CODE RUB: Minimal changes to configure RESTFulSence authorized factory client.

--- RESTFulSense.WASM.Auth/Client/Program.cs
+++ RESTFulSense.WASM.Auth/Client/Program.cs
@@ -2,10 +2,13 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using RESTFulSense.WASM.Auth.Client;
+using RESTFulSense.WebAssembly.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped<IRESTFulApiClientFactory, RESTFulApiClientFactory>();
 
 builder.Services.AddHttpClient("RESTFulSense.WASM.Auth.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
     .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
